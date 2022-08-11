28546e8a6b5671edb41f15fe8c0ced591b5ae330
adarshzpatel@gmail.com <adarshzpatel@gmail.com>
2022-08-12 03:58:12 +0530

    fixed err

--- components/profile/NftCard.tsx
+++ components/profile/NftCard.tsx
@@ -14,11 +14,11 @@ const NftCard = ({ name, imgSrc, description,amount }: Props) => {
     <Tooltip content="Click to view details">
       <div className="rounded-lg hover:border-gray-600 max-h-min cursor-pointer hover:shadow-xl hover:-translate-y-2 duration-200 ease-out border p-3">
         <div className="aspect-square rounded-md flex items-center justify-center bg-gray-100 shadow-inner overflow-hidden">
-          <img
+          {imgSrc && <img
             className=" object-cover object-center w-full"
             src={ipfsToGatewayLink(imgSrc)}
             alt="nft-image"
-          />
+          />}
         </div>
         <p className="font-medium  mt-4">{name}</p>
         <p className="text-gray-400 text-sm mt-1 whitespace-pre-wrap">
