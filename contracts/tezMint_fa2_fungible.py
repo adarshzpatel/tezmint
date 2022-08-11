64490f69ee34cf348c1d5a8758fd4bcc57b32b2e
adarshzpatel@gmail.com <adarshzpatel@gmail.com>
2022-08-11 21:44:32 +0530

    updated fa2 fungible contract code

--- contracts/tezMint_fa2_fungible.py
+++ contracts/tezMint_fa2_fungible.py
@@ -7,37 +7,13 @@ class TezMintSFTCollection(FA2.Admin, FA2.Fa2Fungible):
         FA2.Admin.__init__(self, sp.address("tz1bKyWHn17KyS1HApH1c8rrKaoorKkCWqQV"))
 
     @sp.entry_point
-    def mint(self, batch):
-        """Admin can mint tokens."""
-        sp.set_type(
-            batch,
-            sp.TList(
-                sp.TRecord(
-                    to_=sp.TAddress,
-                    token=sp.TVariant(
-                        new=sp.TMap(sp.TString, sp.TBytes), existing=sp.TNat
-                    ),
-                    amount=sp.TNat,
-                ).layout(("to_", ("token", "amount")))
-            ),
-        )
-        sp.verify(self.is_administrator(sp.sender), "FA2_NOT_ADMIN")
-        with sp.for_("action", batch) as action:
-            with action.token.match_cases() as arg:
-                with arg.match("new") as metadata:
-                    token_id = sp.compute(self.data.last_token_id)
-                    self.data.token_metadata[token_id] = sp.record(
-                        token_id=token_id, token_info=metadata
-                    )
-                    self.data.supply[token_id] = action.amount
-                    self.data.ledger[(action.to_, token_id)] = action.amount
-                    self.data.last_token_id += 1
-                with arg.match("existing") as token_id:
-                    sp.verify(self.is_defined(token_id), "FA2_TOKEN_UNDEFINED")
-                    self.data.supply[token_id] += action.amount
-                    from_ = (action.to_, token_id)
-                    self.data.ledger[from_] = (
-                        self.data.ledger.get(from_, 0) + action.amount
-                    )
+    def mint(self, params):
+        sp.set_type(params,sp.TRecord(to_=sp.TAddress,token=sp.TMap(sp.TString, sp.TBytes),amount=sp.TNat))
+        token_id = sp.compute(self.data.last_token_id)
+        self.data.token_metadata[token_id] = sp.record(token_id=token_id, token_info=params.token)
+        self.data.supply[token_id] = params.amount
+        self.data.ledger[(params.to_, token_id)] = params.amount
+        self.data.last_token_id += 1
+               
 
 sp.add_compilation_target("Tez Mint SFT Collection",TezMintSFTCollection())
